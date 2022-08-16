d537c34e5dfb53c186229ee270816d507b1d5fb8
Clari Ursini <clari_ursini@hotmail.com>
2022-08-16 17:43:00 -0300

    Updated app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title.to_s,
+    content: Faker::Quote.famous_last_words.to_s
+  )
+  article.save
+end
